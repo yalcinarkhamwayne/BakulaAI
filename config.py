b8efe4bc5f79bb4ef8d2665a28d9530e6ca0c181
yalcinarkhamwayne <yalcinarkhamwayne@gmail.com>
2025-03-02 04:08:05 +0100

    aaa

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "mysql+pymysql://bakulaai:bakulaai@192.168.2.20/bakulaai"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "zonguldak-Osch_§27012019")  # Falls Login-Session nötig
