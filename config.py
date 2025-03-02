f28b01c69ba97c4b1913da97120dbe254b66c9cd
yalcinarkhamwayne <yalcinarkhamwayne@gmail.com>
2025-03-02 01:33:07 +0100

    conf

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "bakulaai"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://bakulaai:bakulaai@192.168.2.241/bakulaai"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://bakulaai:bakulaai@192.168.2.249/bakulaai"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
