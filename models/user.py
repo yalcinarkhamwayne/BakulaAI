f2bd42000d39e07806b6a4050dbf05e1f6a87785
yalcinarkhamwayne <yalcinarkhamwayne@gmail.com>
2025-03-02 04:40:26 +0100

    dfgh

--- models/user.py
+++ models/user.py
@@ -1,19 +1,14 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from datetime import datetime
+from app import db
 
-# Benutzerklasse für Flask-Login
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-
-    # Protokolle des Benutzers
-    protocols = db.relationship('Protocol', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-
